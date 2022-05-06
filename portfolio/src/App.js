5dbf68ff83934852f95d63ed10abd07067ff53f3
Brendan Woo <woo.brendan@gmail.com>
2022-05-06 14:49:52 -0400

    import icon component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
+import IconLinks from './components/Icon_links';
 
 function App() {
   return (
     <div className="App">
       <h1>Brendan Woo</h1>
       <h3>Full Stack Developer</h3>
+      <IconLinks />
     </div>
   );
 }
