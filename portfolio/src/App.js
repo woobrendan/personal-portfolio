302b0e244922431e2c3bd2fcab96489b759f27ac
Brendan Woo <woo.brendan@gmail.com>
2022-05-06 16:20:45 -0400

    change icon component to sidebar, import sidebar

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import IconLinks from './components/Icon_links';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
       <h1>Brendan Woo</h1>
       <h3>Full Stack Developer</h3>
-      <IconLinks />
+      {/* <IconLinks /> */}
+      <Sidebar />
+      <div id="Contact">
+        Email Me
+      </div>
     </div>
   );
 }
