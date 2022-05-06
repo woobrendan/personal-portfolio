291e781fb122b3477dc8f021982898458feebc50
Brendan Woo <woo.brendan@gmail.com>
2022-05-06 16:36:05 -0400

    add component for about me section

---
+++ portfolio/src/components/About.js
@@ -0,0 +1,11 @@
+import Typography from '@mui/material/Typography';
+
+export default function AboutMe() {
+  return (
+    <>
+    <Typography variant="h3" gutterBottom component="div">
+      About Me
+    </Typography>
+    </>
+  )
+}
