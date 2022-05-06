02451f702ee5908e4166b779cf979ae275941ac0
Brendan Woo <woo.brendan@gmail.com>
2022-05-06 15:10:51 -0400

    set up side bar component

---
+++ portfolio/src/components/Sidebar.js
@@ -0,0 +1,10 @@
+import IconLinks from "./Icon_links"
+import '../Styles/nav-bar.scss'
+
+export default function Sidebar() {
+  return (
+    <div className="nav-bar">
+      <IconLinks />
+    </div>
+  )
+}
