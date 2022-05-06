b14ec5b1c526b9b40af595711c5da4b02ce6e826
Brendan Woo <woo.brendan@gmail.com>
2022-05-06 15:11:21 -0400

    remove email me button

--- portfolio/src/components/Icon_links.js
+++ portfolio/src/components/Icon_links.js
@@ -6,7 +6,6 @@ export default function IconLinks() {
     <div className="icon-list">
       <SocialIcon url="https://www.linkedin.com/in/woobrendan/" />
       <SocialIcon url="https://github.com/woobrendan" bgColor="#FFFFFF"/>
-      <SocialIcon network="mailto" bgColor="#FFFFFF"/>
     </div>
   )
 }
