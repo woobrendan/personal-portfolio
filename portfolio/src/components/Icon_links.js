ee503a211602aab7ab73ee987067ca168b569c34
Brendan Woo <woo.brendan@gmail.com>
2022-05-06 16:20:21 -0400

    change icons to font awesome, add email icon with link to contact me section

--- portfolio/src/components/Icon_links.js
+++ portfolio/src/components/Icon_links.js
@@ -1,11 +1,47 @@
-import { SocialIcon } from "react-social-icons"
-import '../Styles/icon-list.css'
+
+import '../Styles/icon-list.scss'
+import {faLinkedin, faGithub, faFortAwesome} from '@fortawesome/free-brands-svg-icons'
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+
+library.add(faEnvelope);
+
 
 export default function IconLinks() {
   return(
     <div className="icon-list">
-      <SocialIcon url="https://www.linkedin.com/in/woobrendan/" />
-      <SocialIcon url="https://github.com/woobrendan" bgColor="#FFFFFF"/>
+      <nav>
+      <ul>
+        <li>
+          <a 
+            target="_blank" 
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/woobrendan/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </li>
+        <li>
+          <a 
+            target="_blank" 
+            rel="noreferrer"
+            href="https://github.com/woobrendan"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </li>
+        <li>
+          <a 
+            target="_blank" 
+            rel="noreferrer"
+            href="#Contact"
+          >
+            <FontAwesomeIcon icon="envelope" />
+          </a>
+        </li>
+      </ul>
+      </nav>
     </div>
   )
 }
