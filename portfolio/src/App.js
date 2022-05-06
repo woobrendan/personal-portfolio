9fe9013fdefb10eb98cf4b5b78e9043192a2338a
Brendan Woo <woo.brendan@gmail.com>
2022-05-06 16:35:53 -0400

    add about me component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import AboutMe from './components/About';
 import Sidebar from './components/Sidebar';
 
 function App() {
@@ -7,6 +8,7 @@ function App() {
       <h1>Brendan Woo</h1>
       <h3>Full Stack Developer</h3>
       <Sidebar />
+      <AboutMe />
       <div id="Contact">
         Email Me
       </div>
