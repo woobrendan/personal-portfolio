74ee93fb37a437346dcf99105ced0acc6c66d6e0
Brendan Woo <woo.brendan@gmail.com>
2022-05-09 16:09:20 -0400

    add div for cards vs all about me

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -11,44 +11,46 @@ import WorkIcon from '@mui/icons-material/Work';
 export default function AboutMe() {
   return (
     <div className="About-container">
-      <Typography variant="h3" gutterBottom component="div">
-        About Me
-      </Typography>
-      <div className="Card-list">
-        <Card sx={{ maxWidth: 345 }}>
-          <CardContent>
-            <SchoolIcon />
-            <Typography gutterBottom variant="h4" component="div">
-              Education
-            </Typography>
-            <ul>
-              <li className="school">Lighthouse Labs</li>
-                <ul>
-                  <li>Full Stack Web Development</li>
-                </ul>
-                <br></br>
-              <li className="school">Brock University</li>
-                <ul>
-                  <li>Sport Management</li>
-                </ul>
-            </ul>
-          </CardContent>
-        </Card>
-        <Card sx={{ maxWidth: 345 }}>
-          <CardContent>
-            <WorkIcon />
-            <Typography gutterBottom variant="h4" component="div">
-              Working Experience
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              7+ Years in Professional Sports
-            </Typography>
-            <ul>
-              <li>SRO Motorsports America</li>
-              <li>Toronto Blue Jays</li>
-            </ul>
-          </CardContent>
-        </Card>
+      <div className="About-cards">
+        <Typography variant="h3" gutterBottom component="div">
+          About Me
+        </Typography>
+        <div className="Card-list">
+          <Card sx={{ maxWidth: 345 }}>
+            <CardContent>
+              <SchoolIcon />
+              <Typography gutterBottom variant="h4" component="div">
+                Education
+              </Typography>
+              <ul>
+                <li className="school">Lighthouse Labs</li>
+                  <ul>
+                    <li>Full Stack Web Development</li>
+                  </ul>
+                  <br></br>
+                <li className="school">Brock University</li>
+                  <ul>
+                    <li>Sport Management</li>
+                  </ul>
+              </ul>
+            </CardContent>
+          </Card>
+          <Card sx={{ maxWidth: 345 }}>
+            <CardContent>
+              <WorkIcon />
+              <Typography gutterBottom variant="h4" component="div">
+                Working Experience
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                7+ Years in Professional Sports
+              </Typography>
+              <ul>
+                <li>SRO Motorsports America</li>
+                <li>Toronto Blue Jays</li>
+              </ul>
+            </CardContent>
+          </Card>
+        </div>
       </div>
     </div>
   )
