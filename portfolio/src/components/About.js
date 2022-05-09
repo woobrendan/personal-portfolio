ad8b42f4cca97da5b2e7ef286d79c5abebbfbd9d
Brendan Woo <woo.brendan@gmail.com>
2022-05-09 16:21:18 -0400

    add self description section

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -51,6 +51,17 @@ export default function AboutMe() {
             </CardContent>
           </Card>
         </div>
+        <Card sx={{ maxWidth: 465 }} id="about-description">
+            <CardContent >
+              <SchoolIcon />
+              <Typography gutterBottom variant="h4" component="div">
+                Life of Brendan
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+              </Typography>
+            </CardContent>
+          </Card>
       </div>
     </div>
   )
