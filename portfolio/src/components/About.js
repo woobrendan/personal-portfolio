fa768d971d5b388d1c1d61d5f986479bd4dbe29c
Brendan Woo <woo.brendan@gmail.com>
2022-05-09 13:40:48 -0400

    add card template for education section

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,11 +1,34 @@
 import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
 
 export default function AboutMe() {
   return (
-    <>
+    <div className="About-container">
     <Typography variant="h3" gutterBottom component="div">
       About Me
     </Typography>
-    </>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+        <CardMedia
+          component="img"
+          height="140"
+          image="https://g.foolcdn.com/editorial/images/559462/gettyimages-1059510610.jpg"
+          alt="green iguana"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            Education
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+    </Card>
+    </div>
   )
 }
