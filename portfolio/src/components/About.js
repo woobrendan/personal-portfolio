b1c3a83ca7c4bb1fe503351d85d67661d0e3b4bb
Brendan Woo <woo.brendan@gmail.com>
2022-05-09 16:04:12 -0400

    fix indentation and remove unused imports

--- portfolio/src/components/About.js
+++ portfolio/src/components/About.js
@@ -1,8 +1,6 @@
 import Typography from '@mui/material/Typography';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import { CardActionArea } from '@mui/material';
 import '../Styles/about.scss'
 import SchoolIcon from '@mui/icons-material/School';
 import WorkIcon from '@mui/icons-material/Work';
@@ -18,38 +16,38 @@ export default function AboutMe() {
       </Typography>
       <div className="Card-list">
         <Card sx={{ maxWidth: 345 }}>
-            <CardContent>
-              <SchoolIcon />
-              <Typography gutterBottom variant="h4" component="div">
-                Education
-              </Typography>
-              <ul>
-                <li className="school">Lighthouse Labs</li>
-                  <ul>
-                    <li>Full Stack Web Development</li>
-                  </ul>
-                  <br></br>
-                <li className="school">Brock University</li>
-                  <ul>
-                    <li>Sport Management</li>
-                  </ul>
-              </ul>
-            </CardContent>
+          <CardContent>
+            <SchoolIcon />
+            <Typography gutterBottom variant="h4" component="div">
+              Education
+            </Typography>
+            <ul>
+              <li className="school">Lighthouse Labs</li>
+                <ul>
+                  <li>Full Stack Web Development</li>
+                </ul>
+                <br></br>
+              <li className="school">Brock University</li>
+                <ul>
+                  <li>Sport Management</li>
+                </ul>
+            </ul>
+          </CardContent>
         </Card>
         <Card sx={{ maxWidth: 345 }}>
-            <CardContent>
-              <WorkIcon />
-              <Typography gutterBottom variant="h4" component="div">
-                Working Experience
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                7+ Years in Professional Sports
-              </Typography>
-              <ul>
-                <li>SRO Motorsports America</li>
-                <li>Toronto Blue Jays</li>
-              </ul>
-            </CardContent>
+          <CardContent>
+            <WorkIcon />
+            <Typography gutterBottom variant="h4" component="div">
+              Working Experience
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              7+ Years in Professional Sports
+            </Typography>
+            <ul>
+              <li>SRO Motorsports America</li>
+              <li>Toronto Blue Jays</li>
+            </ul>
+          </CardContent>
         </Card>
       </div>
     </div>
