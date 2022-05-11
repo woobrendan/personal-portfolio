705714d67cc33fd988eb27210639a43f8bbcec33
Brendan Woo <woo.brendan@gmail.com>
2022-05-11 13:55:49 -0400

    set up template for first card

--- portfolio/src/components/experience.js
+++ portfolio/src/components/experience.js
@@ -1,9 +1,32 @@
+import { Typography } from "@mui/material"
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import autograph from '../images/Laguna-Autograph.jpg'
+
 export default function Experience () {
   return (
     <div className="experience-container">
-      <>
-      Work Experience
-      </>
+      <Typography variant="h3" gutterBottom component="div">
+        Work Experience
+      </Typography>
+      <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        height="140"
+        image={autograph}
+        alt="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          SRO Motorsports America
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Lizards are a widespread group of squamate reptiles, with over 6,000
+          species, ranging across all continents except Antarctica
+        </Typography>
+      </CardContent>
+    </Card>
     </div>
   )
 }
