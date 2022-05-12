b26caf04f9728dbf7d0635eaf469a81936fd4682
Brendan Woo <woo.brendan@gmail.com>
2022-05-12 14:55:53 -0400

    fix props call to use object, remove maxwidth from card, import style sheet

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -5,27 +5,31 @@ import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import GitHubIcon from '@mui/icons-material/GitHub';
+import '../Styles/project.scss'
  
  function Project(props) {
    return (
     <div className="project-container">
-       <Card sx={{ maxWidth: 345 }}>
+       <Card>
         <CardMedia
           component="img"
           height="140"
-          image={props.gif}
-          alt={props.name}
+          image={props.project.gif}
+          alt={props.project.name}
         />
         <CardContent>
           <Typography gutterBottom variant="h5" component="div">
-            {props.name}
+            {props.project.name}
           </Typography>
           <Typography variant="body2" color="text.secondary">
-            {props.description}
+            {props.project.description}
+            <br></br>
+            <br></br>
+            Frameworks: {props.project.frameworks}
           </Typography>
         </CardContent>
         <CardActions>
-          <Button variant="outlined" href={props.link} startIcon={<GitHubIcon />}>
+          <Button variant="outlined" href={props.project.link} startIcon={<GitHubIcon />}>
             Code
           </Button>
         </CardActions>
