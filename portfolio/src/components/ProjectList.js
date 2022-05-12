26034251aaa4058e2eae88dfac611674c2df31a8
Brendan Woo <woo.brendan@gmail.com>
2022-05-12 14:56:25 -0400

    add key to mapped projects, give div container a class name to style

--- portfolio/src/components/ProjectList.js
+++ portfolio/src/components/ProjectList.js
@@ -2,11 +2,11 @@ import {projects} from './projectInfo'
 import Project from './Project'
 
 function ProjectList() {
-  const mappedProjects = projects.map(project => (
-    <Project project={project} />
+  const mappedProjects = projects.map((project, index) => (
+    <Project project={project} key={index}/>
   ))
   return (
-    <div>
+    <div className="project-list">
       {mappedProjects}
     </div>
   )
