af8ef62c00f9cf24a8bc598896664c06d0cf60c3
Brendan Woo <woo.brendan@gmail.com>
2022-05-12 13:50:43 -0400

    set template for first prroject

---
+++ portfolio/src/components/projectInfo.js
@@ -0,0 +1,8 @@
+const projects = [
+  {
+    name: "Work It Out",
+    gif: "placeholde",
+    link: "https://github.com/woobrendan/Work-It-Out",
+    description: "Work-It-Out is an app created from scratch as a final project for bootcamp students. Students are given the freedom to choose an idea for an app and to use a tech stack of their choosing. Work-It-Out uses ReactJS for the front-end, Express on the back-end and Postgres for database management.",
+  }
+]
