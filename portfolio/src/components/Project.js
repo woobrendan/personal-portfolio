6a39f9429ffdf32fd3fefa4d42d25950c88c2d49
Brendan Woo <woo.brendan@gmail.com>
2022-05-12 13:51:04 -0400

    import card with media, set up props intake and set up button

--- portfolio/src/components/Project.js
+++ portfolio/src/components/Project.js
@@ -1,10 +1,39 @@
- import React from 'react'
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
+import GitHubIcon from '@mui/icons-material/GitHub';
  
- function Project() {
+ function Project(props) {
    return (
-     <div>
-       
-     </div>
+    <div className="project-container">
+       <Card sx={{ maxWidth: 345 }}>
+        <CardMedia
+          component="img"
+          height="140"
+          image={props.gif}
+          alt={props.name}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.name}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.description}
+          </Typography>
+        </CardContent>
+        <CardActions>
+        <Stack direction="row" spacing={2}>
+          <Button variant="outlined" startIcon={<GitHubIcon />}>
+            Delete
+          </Button>
+        </Stack>
+        </CardActions>
+      </Card>
+    </div>
    )
  }
  
