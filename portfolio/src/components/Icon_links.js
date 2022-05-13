efb579e078e26a9bf3b6eedb6a1631c5c8ff59f0
Brendan Woo <woo.brendan@gmail.com>
2022-05-13 15:47:00 -0400

    add two new icons to list on side bar

--- portfolio/src/components/Icon_links.js
+++ portfolio/src/components/Icon_links.js
@@ -3,6 +3,8 @@ import {faLinkedin, faGithub} from '@fortawesome/free-brands-svg-icons'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import WorkHistoryIcon from '@mui/icons-material/WorkHistory';
+import StickyNote2Icon from '@mui/icons-material/StickyNote2';
 
 library.add(faEnvelope);
 
@@ -11,35 +13,45 @@ export default function IconLinks() {
   return(
     <div className="icon-list">
       <nav>
-      <ul>
-        <li>
-          <a 
-            target="_blank" 
-            rel="noreferrer"
-            href="https://www.linkedin.com/in/woobrendan/"
-          >
-            <FontAwesomeIcon icon={faLinkedin} />
-          </a>
-        </li>
-        <li>
-          <a 
-            target="_blank" 
-            rel="noreferrer"
-            href="https://github.com/woobrendan"
-          >
-            <FontAwesomeIcon icon={faGithub} />
-          </a>
-        </li>
-        <li>
-          <a 
-            target="_blank" 
-            rel="noreferrer"
-            href="#Contact"
-          >
-            <FontAwesomeIcon icon="envelope" />
-          </a>
-        </li>
-      </ul>
+        <ul>
+          <li>
+            <a 
+              target="_blank" 
+              rel="noreferrer"
+              href="https://www.linkedin.com/in/woobrendan/"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+            </a>
+          </li>
+          <li>
+            <a 
+              target="_blank" 
+              rel="noreferrer"
+              href="https://github.com/woobrendan"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+            </a>
+          </li>
+          <li>
+            <a 
+              target="_blank" 
+              rel="noreferrer"
+              href="#Contact"
+            >
+              <FontAwesomeIcon icon="envelope" />
+            </a>
+          </li>
+          <li>
+            <a href="#Experience">
+              <WorkHistoryIcon fontSize="large"/>
+            </a>
+          </li>
+          <li>
+              <a href="#project-list">
+                <StickyNote2Icon fontSize="large"/>
+              </a>
+          </li>
+        </ul>
       </nav>
     </div>
   )
