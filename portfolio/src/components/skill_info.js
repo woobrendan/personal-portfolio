147b65cbd1a770fb181fdc100422f9cf9150a2cb
Brendan Woo <woo.brendan@gmail.com>
2022-05-16 21:09:50 -0500

    reformat data into array of objects, fill out info

--- portfolio/src/components/skill_info.js
+++ portfolio/src/components/skill_info.js
@@ -1,5 +1,14 @@
-const skills = {
-  frontEnd: [],
-  backEnd: [],
-  softSkills: []
-}
+export const skills = [
+  {
+    title: "Languages", 
+    list: ["JavaScript", "Ruby", "HTML", "CSS", "SQL"]
+  },
+  {
+    title: "Frameworks/Libraries", 
+    info: ["Node.js", "React", "Material UI",  "Express", "jQuery", "Bootstrap", "SASS", "Rails", "Active Record", "PostgreSQL"]
+  },
+  {
+    title: "Testing",
+    info: ["Cypress", "Jest", "Storybook", "Jest", "Mocha"]
+  }
+]
