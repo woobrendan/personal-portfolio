5afabe079b1e0d9fd7cc10901f2ee8bad802d83f
Brendan Woo <woo.brendan@gmail.com>
2022-05-16 21:28:07 -0500

    add function to map through skills, return list into component

--- portfolio/src/components/Skills.js
+++ portfolio/src/components/Skills.js
@@ -4,6 +4,12 @@ import CardContent from '@mui/material/CardContent';
 import { skills } from "./skill_info";
 
 function Skills() {
+  const mappedSkillList = (someArray) => {
+    const skillList = someArray.map((skill, index) => (
+        <li key={index}>{skill}</li>
+    ))
+      return skillList;
+  }
 
   const mappedSkills = skills.map((skill, index) => (
     <Card >
@@ -11,23 +17,8 @@ function Skills() {
       <Typography gutterBottom variant="h5" component="div">
         {skill.title}
       </Typography>
-      <Typography gutterBottom variant="subtitle1" component="div">
-        Assistant to Coordinating Member
-      </Typography>
-      <div className="work-info">
-        <p>Location: Toronto, ON</p>
-        <p>Sept 2021 - Dec 2021</p>
-      </div>
-      <ul className="job-description-list">
-        <li><span>
-          Worked a casual contract supporting multiple teams and managers with administrative duties such as  facilitation of official documents, taking meeting notes and manage member absence line
-        </span></li>
-        <li><span>
-          Coordinated manager's schedules, ensuring days and meetings are organized
-        </span></li>
-        <li><span>
-          Facilitated member hearing schedules and caseloads, updating HR software as required
-        </span></li>
+      <ul className="skill-info-list">
+        {mappedSkillList(skill.info)}
       </ul>
     </CardContent>
   </Card>
@@ -37,6 +28,7 @@ function Skills() {
       <Typography variant="h3" gutterBottom component="div">
         Skills
       </Typography>
+      {mappedSkills}
     </div>
   )
 }
